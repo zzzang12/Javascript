1a7efd2c953c3343f3cf369db5ee69949f66ad4e
zzzang12 <sud0@kakao.com>
2022-10-23 01:26:59 +0900

    221022

---
+++ momentum/js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const targetImage = images[Math.floor(Math.random() * images.length)];
+
+const backgroundImage = document.createElement("img");
+
+backgroundImage.src = `img/${targetImage}`;
+
+document.body.appendChild(backgroundImage);
