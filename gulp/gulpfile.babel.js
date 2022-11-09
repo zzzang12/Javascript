7099cc1f9ace5e4a5dd953b62e06a879dd274eaa
zzzang12 <sud0@kakao.com>
2022-11-09 23:43:53 +0900

    221109

--- gulp/gulpfile.babel.js
+++ gulp/gulpfile.babel.js
@@ -1,14 +1,41 @@
 import gulp from "gulp";
-import gulpPug from "gulp-pug";
+import gPug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+import gImage from "gulp-image";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
 };
 
-export const pug = () =>
-  gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest));
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gPug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+const clean = () => del([routes.pug.dest]);
+
+const webserver = () =>
+  gulp.src(routes.pug.dest).pipe(ws({ livereload: true, open: true }));
+
+const img = () =>
+  gulp.src(routes.img.src).pipe(gImage()).pipe(gulp.dest(routes.img.dest));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
